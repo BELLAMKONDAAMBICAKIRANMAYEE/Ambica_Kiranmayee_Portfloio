efd78f57e4df3d50c7b24895f1da2756821d53d3
BELLAMKONDAAMBICAKIRANMAYEE <ambica930@gmail.com>
2025-07-28 19:36:24 +0530

    first commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'ambica930@gmail.com',
+    phone: '+916303694787',
+
+    address: 'Vijayawada,N.T.R District, Andhra Pradesh, India - 520012',
+    github: 'https://github.com/BELLAMKONDAAMBICAKIRANMAYEE',
+    facebook: 'https://www.facebook.com/profile.php?id=100007232299482',
+    linkedIn: 'https://www.linkedin.com/in/ambica-kiranmayee-bellamkonda-203861200/',
+    twitter: '',
+    devUsername: "BELLAMKONDAAMBICAKIRANMAYEE"
+}
